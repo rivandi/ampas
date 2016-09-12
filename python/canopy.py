e307946b202cf22787961f48671b1f5f3d71028e
riyadd <riyad.rivandi@gmail.com>
2016-09-12 14:22:45 +0700

    Better comment format

--- python/canopy.py
+++ python/canopy.py
@@ -9,24 +9,34 @@ from collections import defaultdict
 from mathlib import cosine_similarity
 
 def calculate_similarity(history):
-	"""
-	Function: calculate_similarity
-	Summary: calculate cosine similarity between user
-	Examples: 
-	Attributes:
-		@param (history):
+	"""calculate cosine similarity between user
+	
+	[description]
+	
+	Arguments:
+		history {dictionary of list of binary} --
+			key <- user_id
+			value <- list of user-item interaction (binary)
+			example: 
 			{
 				'u1':[0,1,1,1,0],
 				'u2':[0,1,1,1,0],
 				'u3':[0,1,1,1,0]
 			}
 	Returns:
-		{
-			'u1': {'u2': 0.4, 'u3': 0.5},
-			'u2': {'u1': 0.4, 'u3': 0.6},
-			'u3': {'u1': 0.5, 'u3': 0.6},
-		}
-	"""	
+		dictionary of similarities --
+			key <- user_id_1
+			value <- dictionary of float
+				key <- user_id_2
+				value <- cosine similarity between history of user_id_1 and user_id_2
+			example: 
+			{
+				'u1': {'u2': 0.4, 'u3': 0.5},
+				'u2': {'u1': 0.4, 'u3': 0.6},
+				'u3': {'u1': 0.5, 'u3': 0.6},
+			}
+	"""
+
 	result = defaultdict(dict)
 	users = list(history.keys())
 	for i, user1 in enumerate(users):
@@ -38,26 +48,26 @@ def calculate_similarity(history):
 			result[user2][user1] = distance
 	return result
 
-def cluster_canopy(user_similarity, loose, tight):
-	"""
-	Function: cluster_canopy
-	Summary: 
-		cluster user based on similarity, canopy style
-	Examples:
-	Attributes: 
-		@param (user_similarity):{
-			'u1': {'u2': 0.4, 'u3': 0.5},
-			'u2': {'u1': 0.4, 'u3': 0.6},
-			'u3': {'u1': 0.5, 'u3': 0.6},
-		}
-		@param (loose): float, lower bound
-		@param (tight): float, upper bound
-	Returns: {
-		0: [1, 2, 3, 6, 7, 8, 9],
-		1: [2, 3, 5, 6, 8, 9],
-		2: [8, 9, 4, 5],
-		3: [8, 5, 6]
-	}
+def cluster_canopy(user_similarity, loose=0.3, tight=0.6):
+	"""cluster user based on similarity, canopy style
+	
+	Arguments:
+		user_similarity {dictionary of similarities} -- output of calculate_similarity
+	
+	Keyword Arguments:
+		loose {number} -- lower bound for outer cluster (default: {0.3})
+		tight {number} -- lower bound for inner cluster (default: {0.6})
+	
+	Returns:
+		dictionary of list of user_id --
+			key <- cluster id
+			value <- list of cluster member
+			{
+				0: [1, 2, 3, 6, 7, 8, 9],
+				1: [2, 3, 5, 6, 8, 9],
+				2: [8, 9, 4, 5],
+				3: [8, 5, 6]
+			}
 	"""
 
 	if loose > tight:
@@ -94,7 +104,7 @@ if __name__ == '__main__':
 	item_number = 10
 	loose = 0.4
 	tight = 0.7
-	
+
 	for u in range(user_number):
 		h = [random.randint(0, 1) for y in range(item_number)]
 		history[u] = h
