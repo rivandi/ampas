3fe3568f0efb89c2cdc5871e994dc40d9b3d5ec6
riyadd <riyad.rivandi@gmail.com>
2016-09-18 22:09:52 +0700

    create histogram from matrix

--- python/local_directional_pattern.py
+++ python/local_directional_pattern.py
@@ -8,3 +8,14 @@ This version provides LDPv option, which account for the contrast information [1
 """
 
 import mathlib
+
+def histogramize(matrix, histogram_size):
+	histogram = [0 for x in range(histogram_size)]
+	for x in matrix:
+		for y in x:
+			if y < histogram_size:
+				histogram[y] += 1
+	return histogram
+
+if __name__ == '__main__':
+	pass
--- python/test/test_local_directional_pattern.py
+++ python/test/test_local_directional_pattern.py
@@ -3,3 +3,19 @@
 import context
 import unittest
 import local_directional_pattern as LDP
+
+class TestLDP(unittest.TestCase):
+
+	def test_histogram(self):
+		m1 = [
+			[1, 2, 5],
+			[4, 5, 7],
+			[2, 6, 0],
+			[3, 1, 4]
+		]
+		histogram_size = 8
+		histogram = LDP.histogramize(m1, histogram_size)
+		self.assertEqual(histogram, [1, 2, 2, 1, 2, 2, 1, 1])
+
+if __name__ == '__main__':
+	unittest.main()
