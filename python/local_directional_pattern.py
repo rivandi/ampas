fd6b4624c8c5076d1b0d635549f1574cd7672bf6
riyadd <riyad.rivandi@gmail.com>
2016-09-18 22:08:45 +0700

    LDP initial commit

---
+++ python/local_directional_pattern.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+Complete rewrite of LDP.py, kinda useful for image edge response extraction.
+This version provides LDPv option, which account for the contrast information [1].
+
+[1] Local Directional Pattern Variance (LDPv): A Robust Feature Descriptor for Facial Expression Recognition
+"""
+
+import mathlib
---
+++ python/test/test_local_directional_pattern.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+import context
+import unittest
+import local_directional_pattern as LDP
