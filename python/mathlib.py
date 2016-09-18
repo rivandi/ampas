20e023af111d85586feac659eb991e765864eecf
riyadd <riyad.rivandi@gmail.com>
2016-09-18 21:39:30 +0700

    divide matrix into several smaller ones

--- python/mathlib.py
+++ python/mathlib.py
@@ -10,6 +10,62 @@ from functools import reduce
 def transpose(matrix):
 	return zip(*matrix)
 
+def divide(matrix, region=(1,1), zero_mode=True):
+	"""Divide 2D array into several regions.
+	
+	The result will be numbered in left-right, top-down manner
+		example:
+			divide([
+				[1, 2, 3],
+				[4, 5, 6],
+				[7, 8, 9],
+				[10,11,12]
+			], region=(2,3))
+		divides vertical and horizontal axis into two and three parts respectively
+		thus yields
+			[[1,4], [2,5], [3,6], [7,10], [8,11], [9,12]].
+		Result length for uneven split will be mapped to its ceiling
+			ex:
+				divide([,2,3,4,5], region=(1,3))
+			zero_mode:
+			 	[[1,2], [3,4], [5,0]]
+			not zero_mode:
+			 	[[1,2], [3,4], [5]]
+	
+	Arguments:
+		matrix {2D array}
+	
+	Keyword Arguments:
+		region {tuple} -- pair of vertical & horizontal area (default: {(1,1)})
+		zero_mode {bool} -- make sure every little matrix has same number of element
+			and replace missing ones with zero
+
+	Returns:
+		list of smaller matrices {2D array}
+	"""
+	result = []
+	ver_area, hor_area = region
+	src_height, src_width = len(matrix), len(matrix[0])
+	res_height, res_width = -(-src_height // ver_area), -(-src_width // hor_area)
+
+	for i, x in enumerate(range(ver_area)):
+		for j, y in enumerate(range(hor_area)):
+
+			# start, end index
+			sv, ev = res_height*i, min(src_height, res_height*(i+1)) # vertical
+			sh, eh = res_width*j, min(src_width, res_width*(j+1)) #horizontal
+
+			if zero_mode:
+				# zero matrix with size of res_height*res_width 
+				mini = [[0 for y in range(res_width)] for x in range(res_height)]
+				for k, m in enumerate(range(sv, ev)):
+					for l, n in enumerate(range(sh, eh)):
+						mini[k][l] = matrix[m][n]
+			else:
+				mini = [matrix[k][sh:eh] for k in range(sv, ev)]
+			result.append(mini)
+	return result
+
 def dot_product(x, y):
 	arr = map(lambda i: x[i] * y[i], range(0, min(len(x), len(y))))
 	res = reduce(lambda a, b: a + b, arr)
@@ -23,9 +79,13 @@ def cosine_similarity(x, y):
 	return dot_product(x, y) / (euclidean_norm(x) * euclidean_norm(y))
 
 def demlo_num(n):
+	if type(n) is not int:
+		return 0
 	return sum(map(lambda x:10**x, range(n)))**2
 
 def binet_fib(n):
+	if type(n) is not int:
+		return 0
 	out = (math.sqrt(5)) * (2 ** n)
 	plus = (1 + math.sqrt(5)) ** n
 	minus = (1 - math.sqrt(5)) ** n
--- python/test/test_mathlib.py
+++ python/test/test_mathlib.py
@@ -4,6 +4,17 @@ import unittest
 
 class TestMathlib(unittest.TestCase):
 
+	def __init__(self, *args, **kwargs):
+		super(TestMathlib, self).__init__(*args, **kwargs)
+		self.m1 = [
+			[1, 2, 3],
+			[4, 5, 6],
+			[7, 8, 9],
+			[10,11,12]
+		]
+		self.m2 = [[0,1,2,3]]
+		self.m3 = [[0],[1],[2],[3]]
+
 	def test_demlo(self):
 		d = mathlib.demlo_num(4)
 		self.assertEqual(d, 1234321)
@@ -20,6 +31,26 @@ class TestMathlib(unittest.TestCase):
 
 		d = mathlib.binet_fib(0)
 		self.assertEqual(d, 0)
+
+	def test_divide_matrix_zero(self):
+		m = mathlib.divide(self.m1, region=(2,2), zero_mode=True)
+		self.assertEqual(m, [[[1, 2], [4, 5]], [[3, 0], [6, 0]], [[7, 8], [10, 11]], [[9, 0], [12, 0]]])
+
+		m = mathlib.divide(self.m2, region=(2,2), zero_mode=True)
+		self.assertEqual(m, [[[0, 1]], [[2, 3]], [[0, 0]], [[0, 0]]])
+
+		m = mathlib.divide(self.m3, region=(2,2), zero_mode=True)
+		self.assertEqual(m, [[[0], [1]], [[0], [0]], [[2], [3]], [[0], [0]]])
+
+	def test_divide_matrix_nonzero(self):
+		m = mathlib.divide(self.m1, region=(2,2), zero_mode=False)
+		self.assertEqual(m, [[[1, 2], [4, 5]], [[3], [6]], [[7, 8], [10, 11]], [[9], [12]]])
+
+		m = mathlib.divide(self.m2, region=(2,2), zero_mode=False)
+		self.assertEqual(m, [[[0, 1]], [[2, 3]], [], []])
+
+		m = mathlib.divide(self.m3, region=(2,2), zero_mode=False)
+		self.assertEqual(m, [[[0], [1]], [[], []], [[2], [3]], [[], []]])
 
 if __name__ == '__main__':
 	unittest.main()
